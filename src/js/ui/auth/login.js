c4d28fbbbe753b6d85659fd68f5261c3e38795cf
EliNygard <eli.nygard@gmail.com>
2024-10-21 14:37:33 +0200

    adds new location href path

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -28,8 +28,7 @@ export async function onLogin(event) {
     const {name} = await api.auth.login(data);
     loginSuccessful = true;
     if (loginSuccessful) {
-      window.location.href = `./`;
-      // window.location.href = `/?view=profile&name=${name}`;
+      window.location.href = `/?view=profile&name=${name}`;
     }
   } catch (error) {
     console.error(error);
