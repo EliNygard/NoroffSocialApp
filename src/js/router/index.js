7b3905e6305afe822492c2cab6f0034729f27ce3
EliNygard <eli.nygard@gmail.com>
2024-10-23 10:06:24 +0200

    change package and router to try fix deploy issue

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -14,52 +14,39 @@
 export default async function router(pathname = window.location.pathname) {
   switch (pathname) {
     case "/":
-    case "/index.html":
       await import("./views/home.js");
       break;
     case "/explore/":
-    case "/explore/index.html":
       await import("./views/explore.js");
       break;
     case "/auth/":
-    case "/auth/index.html":
       await import("./views/auth.js");
       break;
-      case "/auth/login/":
-      case "/auth/login/index.html":
+    case "/auth/login/":
       await import("./views/login.js");
       break;
     case "/auth/register/":
-    case "/auth/register/index.html":
       await import("./views/register.js");
       break;
-    case "/post":
     case "/post/":
-    case "/post/index.html":
       await import("./views/post.js");
       break;
-    case "/post/edit/":
-    case "/post/edit/index.html":
+    case "/post/edit":
       await import("./views/postEdit.js");
       break;
     case "/post/create/":
-    case "/post/create/index.html":
       await import("./views/postCreate.js");
       break;
     case "/profile/":
-    case "/profile/index.html":
       await import("./views/profile.js");
       break;
     case "/profiles/":
-    case "/profiles/index.html":
       await import("./views/profiles.js");
       break;
     case "/profiles/posts/":
-    case "/profiles/posts/index.html":
       await import("./views/postsByProfile.js");
       break;
     case "/profiles/profile/":
-    case "/profiles/profile/index.html":
       await import("./views/singleProfile.js");
       break;
     default:
