b30815e5918ebac539220ac1fbc01c1d17ce6973
EliNygard <eli.nygard@gmail.com>
2024-10-23 09:04:22 +0200

    update paths no dot

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -13,53 +13,53 @@
 
 export default async function router(pathname = window.location.pathname) {
   switch (pathname) {
-    case "./":
-    case "./index.html":
+    case "/":
+    case "/index.html":
       await import("./views/home.js");
       break;
-    case "./explore/":
-    case "./explore/index.html":
+    case "/explore/":
+    case "/explore/index.html":
       await import("./views/explore.js");
       break;
-    case "./auth/":
-    case "./auth/index.html":
+    case "/auth/":
+    case "/auth/index.html":
       await import("./views/auth.js");
       break;
-      case "./auth/login/":
-      case "./auth/login/index.html":
+      case "/auth/login/":
+      case "/auth/login/index.html":
       await import("./views/login.js");
       break;
-    case "./auth/register/":
-    case "./auth/register/index.html":
+    case "/auth/register/":
+    case "/auth/register/index.html":
       await import("./views/register.js");
       break;
-    case "./post":
-    case "./post/":
-    case "./post/index.html":
+    case "/post":
+    case "/post/":
+    case "/post/index.html":
       await import("./views/post.js");
       break;
-    case "./post/edit/":
-    case "./post/edit/index.html":
+    case "/post/edit/":
+    case "/post/edit/index.html":
       await import("./views/postEdit.js");
       break;
-    case "./post/create/":
-    case "./post/create/index.html":
+    case "/post/create/":
+    case "/post/create/index.html":
       await import("./views/postCreate.js");
       break;
-    case "./profile/":
-    case "./profile/index.html":
+    case "/profile/":
+    case "/profile/index.html":
       await import("./views/profile.js");
       break;
-    case "./profiles/":
-    case "./profiles/index.html":
+    case "/profiles/":
+    case "/profiles/index.html":
       await import("./views/profiles.js");
       break;
-    case "./profiles/posts/":
-    case "./profiles/posts/index.html":
+    case "/profiles/posts/":
+    case "/profiles/posts/index.html":
       await import("./views/postsByProfile.js");
       break;
-    case "./profiles/profile/":
-    case "./profiles/profile/index.html":
+    case "/profiles/profile/":
+    case "/profiles/profile/index.html":
       await import("./views/singleProfile.js");
       break;
     default:
