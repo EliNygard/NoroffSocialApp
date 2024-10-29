2ca505d9b83570f5dc9a3ced97308077b3dc2307
EliNygard <eli.nygard@gmail.com>
2024-10-29 19:51:54 +0100

    solve merge conflict

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,14 +1,9 @@
 import api from "../../api/instance.js"
 import { authGuard } from "../../utilities/authGuard";
 import { viewPostsFollowing } from "../../ui/posts/viewPostsFollowing";
-
 import { displayHeader } from "../../ui/global/header.js";
 
-import { onLogout } from "../../ui/auth/logout.js";
-
-
 authGuard();
-onLogout()
 
 const token = api.token
 if(token) {
@@ -20,6 +15,3 @@ if(token) {
     body.appendChild(displayHeader())
     viewPostsFollowing();
 }
-
-console.log("home page");
-
