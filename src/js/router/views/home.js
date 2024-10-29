1cfd8ee7bfb7d2de89dd85316a7afc1680ba99c4
EliNygard <eli.nygard@gmail.com>
2024-10-29 15:05:21 +0100

    adds header template

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,12 +1,19 @@
 import api from "../../api/instance.js"
 import { authGuard } from "../../utilities/authGuard";
 import { viewPostsFollowing } from "../../ui/posts/viewPostsFollowing";
+import { displayHeader } from "../../ui/global/header.js";
 
 authGuard();
 
 const token = api.token
 if(token) {
-    viewPostsFollowing();
+    const body = document.querySelector("body")
+    console.log(body);
+
+    const header = displayHeader()
+    
+    body.appendChild(displayHeader())
+    // viewPostsFollowing();
 }
 
 console.log("home page");
--- src/js/ui/global/header.js
+++ src/js/ui/global/header.js
@@ -1,3 +1,34 @@
-export function displayHeader() {
+import api from "../../api/instance.js"
+
+export async function displayHeader() {
+    const parameterString = window.location.search;
+    console.log(parameterString);
     
+    const searchParameters = new URLSearchParams(parameterString);
+    console.log(searchParameters);
+    
+    const profileName = searchParameters.get("name");
+    console.log(profileName);
+    
+
+    const profile = await api.profiles.readSingleProfile(profileName);
+    console.log(profile);
+    
+
+    const header = document.createElement("header")
+
+    const div = document.createElement("div")
+
+    const imgLogo = document.createElement("img")
+    imgLogo.src = "./images/noroff-logo.png"
+    imgLogo.alt = "Noroff logo"
+
+    const imgProfile = document.createElement("img")
+    imgProfile.src = profile.avatar.url
+
+    div.append(imgLogo, imgProfile)
+    header.appendChild(div)
+    console.log(header);
+    
+    return header
 }
