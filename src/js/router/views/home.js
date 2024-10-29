96935ec709ba0eb401e5ddde59bf03be191cdc75
EliNygard <eli.nygard@gmail.com>
2024-10-29 18:54:28 +0100

    adds functionality to logout btn

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,8 +1,10 @@
 import api from "../../api/instance.js"
 import { authGuard } from "../../utilities/authGuard";
 import { viewPostsFollowing } from "../../ui/posts/viewPostsFollowing";
+import { onLogout } from "../../ui/auth/logout.js";
 
 authGuard();
+onLogout()
 
 const token = api.token
 if(token) {
