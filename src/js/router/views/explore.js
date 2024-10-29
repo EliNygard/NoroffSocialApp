64fbe855716dc64e050bd88f454dde34ab677140
EliNygard <eli.nygard@gmail.com>
2024-10-29 21:49:34 +0100

    updates and adds new generated js header

--- src/js/router/views/explore.js
+++ src/js/router/views/explore.js
@@ -1,5 +1,21 @@
+import api from "../../api/instance.js";
+import { displayHeader } from "../../ui/global/header";
 import { viewPostsAll } from "../../ui/posts/viewPostsAll";
 import { authGuard } from "../../utilities/authGuard";
 
 authGuard();
-viewPostsAll();
+
+async function initializePage() {
+    const token = api.token;
+    if (token) {
+      const headerContainer = document.querySelector("header");
+  
+      const header = await displayHeader();
+  
+      headerContainer.appendChild(header);
+      viewPostsAll();
+    }
+  
+  }
+  
+  initializePage();
--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,17 +1,22 @@
-import api from "../../api/instance.js"
+import api from "../../api/instance.js";
 import { authGuard } from "../../utilities/authGuard";
 import { viewPostsFollowing } from "../../ui/posts/viewPostsFollowing";
 import { displayHeader } from "../../ui/global/header.js";
+import { onLogout } from "../../ui/auth/logout.js";
 
 authGuard();
 
-const token = api.token
-if(token) {
-    const body = document.querySelector("body")
-    console.log(body);
+async function initializePage() {
+  const token = api.token;
+  if (token) {
+    const headerContainer = document.querySelector("header");
 
-    const header = displayHeader()
-    
-    body.appendChild(displayHeader())
+    const header = await displayHeader();
+
+    headerContainer.appendChild(header);
     viewPostsFollowing();
+  }
+
 }
+
+initializePage();
