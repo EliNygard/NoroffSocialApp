bb9d7bc95267c675db329b93cb2c605622b9860b
EliNygard <eli.nygard@gmail.com>
2024-10-29 13:38:08 +0100

    improves ui message on home when user is not logged in

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,8 +1,13 @@
+import api from "../../api/instance.js"
 import { authGuard } from "../../utilities/authGuard";
 import { viewPostsFollowing } from "../../ui/posts/viewPostsFollowing";
 
 authGuard();
-viewPostsFollowing();
+
+const token = api.token
+if(token) {
+    viewPostsFollowing();
+}
 
 console.log("home page");
 
--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -14,14 +14,14 @@ export function authGuard() {
 
 function diplayAuthGuard() {
   const container = document.createElement("div")
-  container.textContent = "You must be logged in to view this page"
+  container.textContent = "Welcome to the Noroff Social Community. Connect with the community of the Noroff Vocational Scool of technology and digital media. Log in to your account or register one to start exploring the community."
   container.classList.add("fullscreen-container")
   
   
   const loginBtn = document.createElement("a")
   loginBtn.textContent = "Log in"
   loginBtn.href = "/NoroffSocialApp/auth/login/"
-  
+
   const registerBtn = document.createElement("a")
   registerBtn.textContent = "Register account"
   registerBtn.href = "/NoroffSocialApp/auth/register/"
