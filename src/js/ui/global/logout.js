db81dbd9a2ddf8aec50d5e4b3cfd22b118b7191c
EliNygard <eli.nygard@gmail.com>
2024-10-30 10:56:51 +0100

    fix: logout

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -11,7 +11,9 @@ import api from "../../api/instance.js";
  */
 
 export function setLogoutListener() {
-  const buttons = document.querySelectorAll("[data-auth=logout]");
+  const button = document.querySelector("[data-auth=logout]");
+  console.log(button);
+  
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       api.auth.logout();
