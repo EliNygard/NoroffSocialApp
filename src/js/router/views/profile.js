b05142c65b571e1700c6f6150d1b6e546f369113
EliNygard <eli.nygard@gmail.com>
2024-11-05 21:46:59 +0100

    adds scroll to update profile form

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -3,41 +3,39 @@ import { displayHeader } from "../../ui/global/header";
 import { onUpdateProfile } from "../../ui/profiles/update";
 import { viewCurrentUser } from "../../ui/profiles/viewCurrentUser";
 import { authGuard } from "../../utilities/authGuard";
-
-authGuard();
+import api from "../../api/instance.js";
 
 async function initializePage() {
-    
+  const token = api.token;
+
+  if (token) {
+    onLogout();
     try {
-        const headerContainer = document.querySelector("header");
+      const headerContainer = document.querySelector("header");
       const header = await displayHeader();
       headerContainer.appendChild(header);
 
-      const updateProfileBtn = document.getElementById("updateProfileBtn")
-      const updateProfileForm = document.getElementById("updateProfile")
+      const updateProfileBtn = document.getElementById("updateProfileBtn");
+      const updateProfileForm = document.getElementById("updateProfile");
       updateProfileBtn.addEventListener("click", () => {
         if (updateProfileForm) {
-            window.scrollTo({
-              top: updateProfileForm.offsetTop + -150,
-              behavior: "smooth",
-            })
-        
-          } else{
-            window.scrollTo(0, 0)
-          }
-      })
+          window.scrollTo({
+            top: updateProfileForm.offsetTop + -150,
+            behavior: "smooth",
+          });
+        } else {
+          window.scrollTo(0, 0);
+        }
+      });
 
-        const form = document.forms.updateProfile;
-        form.addEventListener("submit", onUpdateProfile);
-        viewCurrentUser();
-        onLogout()
+      const form = document.forms.updateProfile;
+      form.addEventListener("submit", onUpdateProfile);
 
+      viewCurrentUser();
     } catch {}
-
+  } else {
+    authGuard();
+  }
 }
 
-initializePage()
-
-
-
-
+initializePage();
