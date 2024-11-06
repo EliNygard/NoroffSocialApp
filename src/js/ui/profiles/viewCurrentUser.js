d7b523ec720a95b9e2209a8d8aa7278cc14b75b8
EliNygard <eli.nygard@gmail.com>
2024-11-06 15:22:35 +0100

    adds styling to profile page

--- src/js/ui/profiles/viewCurrentUser.js
+++ src/js/ui/profiles/viewCurrentUser.js
@@ -21,86 +21,76 @@ export async function viewCurrentUser() {
     const profile = await api.profiles.readSingleProfile(userName);
 
     const profileCard = document.createElement("section");
-    profileCard.className = "sm:text-lg"
+    profileCard.className = "sm:text-lg";
 
     const banner = document.createElement("img");
-    banner.className = "w-full aspect-[3/1] object-cover"
+    banner.className = "w-full aspect-[3/1] object-cover";
     banner.src = profile.banner.url;
 
-    const profileContainer = document.createElement("div")
-    profileContainer.className = "my-4 mx-2 flex items-center gap-4"
+    const profileContainer = document.createElement("div");
+    profileContainer.className = "my-4 mx-2 flex items-center gap-4";
 
     const avatar = document.createElement("img");
-    avatar.className = "rounded-full size-12 object-cover"
+    avatar.className = "rounded-full size-12 object-cover";
     avatar.src = profile.avatar.url;
 
     const name = document.createElement("h2");
     name.textContent = profile.name;
 
-    profileContainer.append(avatar, name)
+    profileContainer.append(avatar, name);
 
     const bio = document.createElement("p");
-    bio.className = 'mb-6 mx-2'
+    bio.className = "mb-6 mx-2";
     bio.textContent = profile.bio;
 
-    const border = document.createElement("div")
-    border.className = "border border-gray-400 mb-6 mx-2"
+    const border = document.createElement("div");
+    border.className = "border border-gray-400 mb-6 mx-2";
 
     const countPosts = document.createElement("p");
-    countPosts.className = "mb-6 mx-2"
+    countPosts.className = "mb-6 mx-2";
     countPosts.textContent = `${profile.name} has written ${profile._count.posts} posts.`;
     const posts = profile.posts;
 
     if (posts) {
       const list = posts.map((post) => {
         console.log(post);
-        
+
         const li = document.createElement("li");
-        li.className = "mx-2"
-
-        // const title = document.createElement("a");
-        // title.textContent = post.title;
-        // title.href = `../post/?id=${post.id}`;
-
-        // const body = document.createElement("p");
-        // body.textContent = post.body;
-
+        li.className = "mx-2";
 
         // The post
-      const postContainer = document.createElement("a");
-      postContainer.className = "flex flex-col gap-3 mb-4";
-      postContainer.href = `/NoroffSocialApp/post/?id=${post.id}`
+        const postContainer = document.createElement("a");
+        postContainer.className = "flex flex-col gap-3 mb-4";
+        postContainer.href = `/NoroffSocialApp/post/?id=${post.id}`;
 
-      const img = document.createElement("img");
-      img.className = "w-full object-contain";
-      if (post.media) {
-        img.src = post.media.url;
-      }
+        const img = document.createElement("img");
+        img.className = "w-full object-contain";
+        if (post.media) {
+          img.src = post.media.url;
+        }
 
-      const postDate = document.createElement("p");
-      postDate.className = "mx-2 text-xs flex justify-end text-stone-700";
-      if (post.updated === post.created) {
-        postDate.textContent = formatDate(post.created);
-      } else {
-        postDate.textContent = formatDate(post.updated);
-      }
+        const postDate = document.createElement("p");
+        postDate.className = "mx-2 text-xs flex justify-end text-stone-700";
+        if (post.updated === post.created) {
+          postDate.textContent = formatDate(post.created);
+        } else {
+          postDate.textContent = formatDate(post.updated);
+        }
 
-      const h3 = document.createElement("h3");
-      h3.className = "mx-2 font-semibold";
-      h3.textContent = post.title;
+        const h3 = document.createElement("h3");
+        h3.className = "mx-2 font-semibold";
+        h3.textContent = post.title;
 
-      const body = document.createElement("p");
-      body.className = "mx-2";
-      body.textContent = post.body;
+        const body = document.createElement("p");
+        body.className = "mx-2";
+        body.textContent = post.body;
 
-      if (img.src) {
-        postContainer.appendChild(img);
-      }
-      postContainer.append(postDate, h3, body);
+        if (img.src) {
+          postContainer.appendChild(img);
+        }
+        postContainer.append(postDate, h3, body);
 
-
-      
-      li.appendChild(postContainer)
+        li.appendChild(postContainer);
         // li.append(title, body);
         return li;
       });
