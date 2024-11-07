5f79a08e2205777ef18278405d8e1c3570413f9f
EliNygard <eli.nygard@gmail.com>
2024-11-07 15:00:34 +0100

    adds font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
